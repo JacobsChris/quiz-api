c96ff2ff88c383e9e5b3277ca7eb9a9e51060477
JacobsChris <58259285+JacobsChris@users.noreply.github.com>
2020-10-21 14:37:14 +0100

    updated app+tests to require 1 player

--- src/tests/quiz.nextStageAsync.spec.js
+++ src/tests/quiz.nextStageAsync.spec.js
@@ -69,11 +69,12 @@ describe('quiz.nextStageAsync()', function () {
             expect(instance.players.map(p => p.score)).to.have.members([0, 0, 0]);
         });    
 
-        it('should reject request if no players yet', async function () {
+        it('should reject request if only one player', async function () {
             instanceBefore.players = [ {name: "Player 1"} ]; // One players
-            await expect(quiz.nextStageAsync('1234')).to.eventually.be.rejectedWith('Not enough players yet');
+            await expect(quiz.nextStageAsync('1234')).to.eventually.be.rejectedWith('Not enough players yet'
+           );
             expect(saveInstance.notCalled).to.be.true;
-        });
+           });
     });
 
     describe('when showing question', function() {
